2d2211a03c2eb9f562667fa9e86a7eef9c37892a
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-07-30 11:34:01 -0600

    add answers to prompts when choosing a Motorbike, push the newly made motorbike to the vehicles array, create a vin and perform motorbike actions

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -253,10 +253,23 @@ class Cli {
         },
       ])
       .then((answers) => {
+        const motorbike = new Motorbike(
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          parseInt(answers.year),
+          parseInt(answers.weight),
+          parseInt(answers.topSpeed),
+          [],
+        );
         // TODO: Use the answers object to pass the required properties to the Motorbike constructor
         // TODO: push the motorbike to the vehicles array
+        this.vehicles.push(motorbike);
         // TODO: set the selectedVehicleVin to the vin of the motorbike
+        this.selectedVehicleVin = motorbike.vin;
         // TODO: perform actions on the motorbike
+        this.performActions();
       });
   }
 
